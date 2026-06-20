69e3364ed89ebb2cbf65cb61cb595b02b10d1980
thiago619 <jurassicpark171@gmail.com>
2026-06-20 13:46:18 -0300

    Requisição com a API e encaminhamento da resposta ao cliente implementados

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import(
 	"fmt"
 	"net/http"
+	"io"
 )
 
 func main(){
@@ -10,16 +11,34 @@ func main(){
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /cotacao",func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("funcionou"))
-	})
+	mux.HandleFunc("GET /cotacao",cotacao)
 
 	err := http.ListenAndServe(":8080",mux)
 
 	if(err != nil){
 		fmt.Println(err)
 	}
+}
 
+func cotacao(w http.ResponseWriter, r *http.Request){
+	client := &http.Client{}
+	url := "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+	req, err := http.NewRequest("GET",url,nil)
+	if err != nil{
+		panic(err)
+	}
+	resp, err := client.Do(req)
+	if err != nil{
+		panic(err)
+	}
+	defer resp.Body.Close()
 
+	data, err := io.ReadAll(resp.Body)
+	if err != nil{
+		panic(err)
+	}
+
+	w.Write(data)
 
 }
+
